2b5ff5a2ec250bab1afe2c2bdcec1bc9193530d1
Jack Green <jack.green@bbzbl-it.ch>
2022-12-08 11:50:38 +0100

    changed to correct dotnet file structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace M346_Projekt_Login.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
