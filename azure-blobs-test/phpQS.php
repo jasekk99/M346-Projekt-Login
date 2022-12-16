eecf17fba0f9cbe002a1e3de57a99deb207f1107
Jack Green <jack.green@bbzbl-it.ch>
2022-12-16 11:02:24 +0100

    PLEASE WORK ISTG

--- azure-blobs-test/phpQS.php
+++ azure-blobs-test/phpQS.php
@@ -153,7 +153,18 @@ else
 }
 ?>
 
-
-<form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
-</form>
+    </form>
+</body>
+</html>
+
